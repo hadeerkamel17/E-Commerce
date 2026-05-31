8388f8beceb49dd9c514eebc7da7021c57935871
Hadeer Kamel <hadeerkamel00@gmail.com>
2026-05-31 19:05:46 +0300

    first commit

---
+++ src/proxy.ts
@@ -0,0 +1,15 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function proxy(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  console.log("from middleware", token);
+  if (!!token) {
+    return NextResponse.next();
+  }
+
+  return NextResponse.redirect(`${process.env.NEXTAUTH_URL}login`);
+}
+export const config = {
+  matcher: ["/cart", "/profile"],
+};
